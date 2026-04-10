407c8e08f9af5d20e406379baa33b3f1835d6a37
Juan Aguilar <zalapeach@gmail.com>
2026-04-10 13:23:12 -0600

    fix error in startup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,2 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - _favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
