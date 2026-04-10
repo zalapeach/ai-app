ec60f874c05f5b4798671c5f6d2e592f98b5b383
Juan Aguilar <zalapeach@gmail.com>
2026-04-10 13:40:40 -0600

    added more configs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { verifySession } from "@/lib/auth";
 
 export async function middleware(request: NextRequest) {
   return NextResponse.next();
