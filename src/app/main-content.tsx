9c797a30143f06cd8cfeda381c42428e34b58144
Juan Aguilar <zalapeach@gmail.com>
2026-04-24 09:12:06 -0600

    start to create panels

--- src/app/main-content.tsx
+++ src/app/main-content.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useState } from "react";
+import {
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { FileSystemProvider } from "@/lib/contexts/file-system-context";
 import { ChatProvider } from "@/lib/contexts/chat-context";
 
@@ -26,6 +30,11 @@ export function MainContent({user, project}: MainContentProps) {
     <FileSystemProvider initialData={project?.data}>
       <ChatProvider projectId={project?.id} initialMessages={project?.messages}>
         <div className="h-screen w-screen overflow-hidden bg-neutral-50">
+          <ResizablePanelGroup direction ="horizontal" className="h-full">
+            {/* Left Panel - Chat */}
+            <ResizablePanel defaultSize={35} minSize={25} maxSize={50}>
+            </ResizablePanel>
+          </ResizablePanelGroup>
         </div>
       </ChatProvider>
     </FileSystemProvider>
---
+++ src/components/ui/resizable.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import * as React from "react"
+import { GripVerticalIcon } from "lucide-react"
+import * as ResizablePrimitive from "react-resizable-panels"
+
+import { cn } from "@/lib/utils"
+
+function ResizablePanelGroup({
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
+  return (
+    <ResizablePrimitive.PanelGroup
+      data-slot="resizable-panel-group"
+      className={cn(
+        "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function ResizablePanel({
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.Panel>) {
+  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />
+}
+
+export { ResizablePanelGroup, ResizablePanel }
